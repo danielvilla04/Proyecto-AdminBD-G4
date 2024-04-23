4ad82aa6a33c29704e5076b084d3769406782747
Fabri1806 <123427636+Fabri1806@users.noreply.github.com>
2024-04-23 12:26:00 -0600

    Add files via upload
    Codigo de creación del schema, codigo de monitoreo, codigo de respaldo y recuperación

---
+++ Respaldo_y_Monitoreo.sql
@@ -0,0 +1,32 @@
+--creacion del squema
+CREATE SCHEMA Pymes;
+ALTER SESSION SET CURRENT_SCHEMA = Pymes;
+
+--Monitoreo
+SELECT sql_id, sql_text, executions, elapsed_time, cpu_time, buffer_gets
+FROM v$sql
+WHERE elapsed_time > 10000 -- Consultas que han tardado más de 10 segundos
+ORDER BY elapsed_time DESC;
+
+
+SELECT s.sid, s.serial#, s.username, s.machine, s.program, s.status,
+       t.sql_id, t.sql_child_number, t.sql_text
+FROM v$session s
+JOIN v$sql t ON s.sql_id = t.sql_id
+WHERE s.status = 'ACTIVE';
+
+--respaldo y recuperación
+
+
+
+CREATE OR REPLACE DIRECTORY respaldo AS 'C:\Backups\Venta_Pymes';
+
+/*
+
+codigo en el CMD para la exportación
+C:\Users\fidelitas>expdp \"/ as sysdba \" directory = respaldo dumpfile = respaldoschema.dmp logfile = respaldoschema.log schemas = system
+
+codigo en el CMD para la importación
+C:\Users\fidelitas>impdp \"/ as sysdba \" directory = respaldo dumpfile = respaldoschema.dmp logfile = importacionlog.log schemas = system: Fabri
+
+*/
